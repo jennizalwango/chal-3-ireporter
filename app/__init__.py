72b98f262cd46ad566586921b267c6cc3da0dfb2
Jenny Zalwango <43774983+jennizalwango@users.noreply.github.com>
2019-03-11 10:55:01 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,13 +8,7 @@ app = Flask(__name__)
 config = configuration.get(os.environ.get("APP_ENV", "development"))
 app.config.from_object(config)
 
-# DatabaseConnection = psycopg2.connect(
-#   database = config.DATABASE_NAME, 
-#   password = config.DATABASE_PASSWORD, 
-#   user = config.DATABASE_USER, 
-#   host = config.DATABASE_HOST, 
-#   port = 5432
-#   )
+
 
 if os.environ.get("database") == 'heroku':
    DatabaseConnection = psycopg2.connect(database = "d7lrmgbjs3a0im",user="qzgoznoygmxmvo", password="da463569b47a5c48c1c7e0b19bf91e3535c4e3ace4bba892260bf9850c9f6e88",host="ec2-23-21-165-188.compute-1.amazonaws.com",port=5432)
